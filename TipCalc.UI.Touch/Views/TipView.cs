fc8df936369613bd2de8a5eb4a8ea0a60e4115ed
Somya Jain <somya.jain@cbsinteractive.com>
2014-04-22 00:41:08 -0400

    completed ios

---
+++ TipCalc.UI.Touch/Views/TipView.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using Cirrious.MvvmCross.Touch.Views;
+using Cirrious.MvvmCross.Binding.BindingContext;
+using TipCalc.Core;
+
+namespace TipCalc.UI.Touch
+{
+	public partial class TipView : MvxViewController
+	{
+		public TipView() : base( "TipView", null )
+		{
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			
+
+			var set = this.CreateBindingSet<TipView, TipViewModel>();
+			set.Bind(SubTotalTextField).To(vm => vm.SubTotal);
+			set.Bind(GenerosiySlider).To(vm => vm.Generosity);
+			set.Bind(TipLabel).To(vm => vm.Generosity);
+			set.Apply();
+
+			this.EdgesForExtendedLayout = UIRectEdge.None;
+		}
+	}
+}
+
---
+++ TipCalc.UI.Touch/Views/TipView.designer.cs
@@ -0,0 +1,42 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+
+namespace TipCalc.UI.Touch
+{
+	[Register ("TipView")]
+	partial class TipView
+	{
+		[Outlet]
+		MonoTouch.UIKit.UISlider GenerosiySlider { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField SubTotalTextField { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel TipLabel { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (SubTotalTextField != null) {
+				SubTotalTextField.Dispose ();
+				SubTotalTextField = null;
+			}
+
+			if (GenerosiySlider != null) {
+				GenerosiySlider.Dispose ();
+				GenerosiySlider = null;
+			}
+
+			if (TipLabel != null) {
+				TipLabel.Dispose ();
+				TipLabel = null;
+			}
+		}
+	}
+}
